4c09d235e0ba9a54d78b469eaf122b70d7cca4ff
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2023-03-06 09:25:53 +0300

    Spring initiliazr

---
+++ src/main/java/com/github/dmitrkuznetsov/lumus_ms/LumusMsApplication.java
@@ -0,0 +1,13 @@
+package com.github.dmitrkuznetsov.lumus_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumusMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumusMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dmitrkuznetsov/lumus_ms/LumusMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dmitrkuznetsov.lumus_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumusMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
